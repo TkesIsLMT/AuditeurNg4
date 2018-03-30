e0dfef45bcdd6ac09b51aaaa449c9f79571763b8
Laurent Moret <Moret_L@TKESIS01.com>
2018-03-30 15:17:27 +0200

    ajout routing de base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { CategorieListComponent } from './business/categories/categorie-list/categorie-list.component';
+
+const routes: Routes=[
+  { path: 'home', component:HomeComponent },
+  { path: 'categories', component: CategorieListComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
+  imports: [
+    BrowserModule, 
+    RouterModule.forRoot(routes)
+  ],
   declarations: [
     AppComponent,
-    NavigationComponent
-  ],
-  imports: [
-    BrowserModule
+    NavigationComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    CategorieListComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
